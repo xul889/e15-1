11322145b9c11ff385139a1b20d59e49045948bd
Susan Buck <susanbuck@fas.harvard.edu>
2022-02-01 15:40:05 +0000

    Added author info readme; initialized practice app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # e15
 Course work for CSCI E-15
+By: Susan Buck
---
+++ practice/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Practice App</title>
+    <meta charset='utf-8'>
+    <link href=data: , rel=icon>
+</head>
+
+<body>
+    <h1>Practice App</h1>
+</body>
+
+</html>
